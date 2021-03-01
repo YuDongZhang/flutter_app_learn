5287ab7b8a0b3f17c376e49e94b7d6de75c9709d
YuDongZhang <977872239@qq.com>
2021-03-01 18:03:23 +0800

    pageview

--- lib/20-宁浩教程练习/demo/view_demo.dart
+++ lib/20-宁浩教程练习/demo/view_demo.dart
@@ -5,7 +5,46 @@ import 'package:flutter/material.dart';
 class ViewDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageViewDemo();
   }
 
+}
+
+///页面视图 ,可以设置一页一页 可以竖向和横向的滑动
+class PageViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PageView(
+      // pageSnapping: false,
+      // reverse: true,
+      // scrollDirection: Axis.vertical,
+      // onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
+      // controller: PageController(
+      //   initialPage: 1,
+      //   keepPage: false,
+      //   viewportFraction: 0.85,
+      // ),
+      children: <Widget>[
+        Container(
+          color: Colors.brown[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text('ONE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        Container(
+          color: Colors.grey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text('TWO',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        Container(
+          color: Colors.blueGrey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text('THREE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+      ],
+    );
+  }
 }
