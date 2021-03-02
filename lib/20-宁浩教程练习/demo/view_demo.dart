85e092e9ac25a20387389802ada6e55885b193cd
YuDongZhang <977872239@qq.com>
2021-03-02 19:19:34 +0800

    PageViewBuilder 和pagerview 差不多 , 可以添加文字

--- lib/20-宁浩教程练习/demo/view_demo.dart
+++ lib/20-宁浩教程练习/demo/view_demo.dart
@@ -1,13 +1,48 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/model/post.dart';
 
-class ViewDemo extends StatelessWidget{
+class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageViewDemo();
+    return PageViewBuilderDemo();
+  }
+}
+
+class PageViewBuilderDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: [
+        SizedBox.expand(
+          //这样 box 尺寸会占满所有的空间
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+            bottom: 8.0, //设置部件底部位置
+            left: 8.0, //设置部件左边的位置
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, //交叉轴的对其
+              children: [
+                Text(
+                  posts[index].title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(posts[index].author,)
+              ],
+            )),
+      ],
+    );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
 }
 
 ///页面视图 ,可以设置一页一页 可以竖向和横向的滑动
@@ -15,14 +50,18 @@ class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      pageSnapping: false,//页面的粘性 , 拖到一半不到会退回去
-      reverse: false,//倒置 , 页面的顺序颠倒
-      scrollDirection: Axis.vertical,//滚动的方向
-      onPageChanged: (currentPage)=> debugPrint('Page : $currentPage'),//可以在控制台打印当前的页面
+      pageSnapping: false,
+      //页面的粘性 , 拖到一半不到会退回去
+      reverse: false,
+      //倒置 , 页面的顺序颠倒
+      scrollDirection: Axis.vertical,
+      //滚动的方向
+      onPageChanged: (currentPage) => debugPrint('Page : $currentPage'),
+      //可以在控制台打印当前的页面
       controller: PageController(
-        initialPage: 1,//默认滚动到 第二 个页面
-        keepPage: false,//是否记住了 , 滚动到那个页面
-        viewportFraction: 0.85,//页面占用 可视图的 比例
+        initialPage: 1, //默认滚动到 第二 个页面
+        keepPage: false, //是否记住了 , 滚动到那个页面
+        viewportFraction: 0.85, //页面占用 可视图的 比例
       ),
       children: <Widget>[
         Container(
