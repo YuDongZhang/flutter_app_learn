664f276a09b3f7290042d3ac7d1749fa02ac05e9
YuDongZhang <977872239@qq.com>
2021-03-03 11:36:49 +0800

    格式化

--- lib/20-宁浩教程练习/demo/sliver_demo.dart
+++ lib/20-宁浩教程练习/demo/sliver_demo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/model/post.dart';
+
 /// sliver 翻译是条子薄片的意思
-class SliverDemo extends StatelessWidget{
+class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,24 +10,25 @@ class SliverDemo extends StatelessWidget{
         slivers: [
           // SliverPadding(padding: EdgeInsets.all(8.0),sliver: SliverGridDemo()),
           // SliverSafeArea(sliver: SliverGridDemo())//显示在安全区域 , 就是避免被刘海挡住
-          SliverSafeArea(sliver: SliverPadding(
-            padding: EdgeInsets.all(8.0),
-            sliver: SliverListDemo(),
-          ),)
+          SliverSafeArea(
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverListDemo(),
+            ),
+          )
         ],
       ),
     );
   }
-
 }
-
 
 class SliverListDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverList(
-      delegate: SliverChildBuilderDelegate(//delegate 代表的意思
-            (BuildContext context, int index) {
+      delegate: SliverChildBuilderDelegate(
+        //delegate 代表的意思
+        (BuildContext context, int index) {
           return Container(
             child: Image.network(
               posts[index].imageUrl,
@@ -40,7 +42,6 @@ class SliverListDemo extends StatelessWidget {
   }
 }
 
-
 class SliverGridDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -49,10 +50,11 @@ class SliverGridDemo extends StatelessWidget {
         crossAxisCount: 2,
         crossAxisSpacing: 8.0,
         mainAxisSpacing: 8.0,
-        childAspectRatio: 1.0,//网格显示的比例
+        childAspectRatio: 1.0, //网格显示的比例
       ),
-      delegate: SliverChildBuilderDelegate(//delegate 代表的意思
-            (BuildContext context, int index) {
+      delegate: SliverChildBuilderDelegate(
+        //delegate 代表的意思
+        (BuildContext context, int index) {
           return Container(
             child: Image.network(
               posts[index].imageUrl,
