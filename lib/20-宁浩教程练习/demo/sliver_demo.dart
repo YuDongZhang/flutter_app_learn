1b39557f02642227afc4474317fe44aa3cadfe0b
YuDongZhang <977872239@qq.com>
2021-03-03 14:12:50 +0800

    设置了图片的圆角 不过没有效果

--- lib/20-宁浩教程练习/demo/sliver_demo.dart
+++ lib/20-宁浩教程练习/demo/sliver_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/model/post.dart';
 
 /// sliver 翻译是条子薄片的意思
@@ -15,6 +16,7 @@ class SliverDemo extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               sliver: SliverListDemo(),
             ),
+
           )
         ],
       ),
@@ -29,11 +31,55 @@ class SliverListDemo extends StatelessWidget {
       delegate: SliverChildBuilderDelegate(
         //delegate 代表的意思
         (BuildContext context, int index) {
-          return Container(
-            child: Image.network(
-              posts[index].imageUrl,
-              fit: BoxFit.cover,
-            ),
+          return Padding(
+            padding: EdgeInsets.only(bottom: 32), //只是添加底边距
+
+            child: Material(
+                borderRadius: BorderRadius.circular(12.0), //实际测试发现无效
+                elevation: 14.0,
+                shadowColor: Colors.grey.withOpacity(0.5),
+                child: Stack(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 16 / 9, //图片的比例
+                      child: Image.network(
+                        posts[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      top: 32.0,
+                      left: 32.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            posts[index].title,
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                          Text(
+                            posts[index].author,
+                            style:
+                                TextStyle(fontSize: 13.0, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+
+                // Container(
+                //   // width: 120,//设置了之后可以看到圆角 ,不过不是图片的
+                //   // height: 60,
+                //   child: Image.network(
+                //     posts[index].imageUrl,
+                //     fit: BoxFit.contain,
+                //   ),
+                //
+                // ),
+
+                ),
           );
         },
         childCount: posts.length,
