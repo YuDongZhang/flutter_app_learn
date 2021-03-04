0989f3332472335261b46ab758df4e93d0eeb16a
YuDongZhang <977872239@qq.com>
2021-03-04 14:42:40 +0800

    文本框可以设置斜线

--- lib/20-宁浩教程练习/demo/form_demo.dart
+++ lib/20-宁浩教程练习/demo/form_demo.dart
@@ -9,7 +9,38 @@ class FormDemo extends StatelessWidget {
         data: ThemeData(
           primaryColor: Colors.black,
         ),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFieldDemo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  @override
+  TextFieldDemoState createState() => TextFieldDemoState();
+}
+
+class TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+
+    return TextField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.subject),//文本框左边的小图标
+        labelText: '我是输入框',
+        hintText: '点击我可以输入',
+        // border: InputBorder.none,//取消了下划线
+        border: OutlineInputBorder(),
+        filled: true,
+        // fillColor:  ,设置背景颜色
       ),
     );
   }
