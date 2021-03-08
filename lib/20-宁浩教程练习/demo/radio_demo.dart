31ba0274d4642ac26516fa423b8690505074ec91
YuDongZhang <977872239@qq.com>
2021-03-08 09:59:51 +0800

    RadioListTile：带标签与图标的单选按钮

--- lib/20-宁浩教程练习/demo/radio_demo.dart
+++ lib/20-宁浩教程练习/demo/radio_demo.dart
@@ -28,22 +28,40 @@ class _RadioDemoState extends State<RadioDemo> {
           children: <Widget>[
             Text('RadioGroupValue: $_radioGroupA'),
             SizedBox(height: 32.0),
-
+            RadioListTile(
+              value: 0,
+              groupValue: _radioGroupA,//群组值
+              onChanged: _handleRadioValueChanged,
+              title: Text('Options A'),
+              subtitle: Text('Description'),
+              secondary: Icon(Icons.filter_1),
+              selected: _radioGroupA == 0,//这个属性是让小图标是否是激活的颜色
+            ),
+            RadioListTile(
+              value: 1,
+              groupValue: _radioGroupA,
+              onChanged: _handleRadioValueChanged,
+              title: Text('Options B'),
+              subtitle: Text('Description'),//副标题
+              secondary: Icon(Icons.filter_2),
+              selected: _radioGroupA == 1,
+            ),
             Row(
+              ///单选按钮的值和群组的值是一样的就是选中的状态
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Radio(
-                  value: 0,
-                  groupValue: _radioGroupA,
-                  onChanged: _handleRadioValueChanged,
-                  activeColor: Colors.black,//被选中的颜色
-                ),
-                Radio(
-                  value: 1,
-                  groupValue: _radioGroupA,
-                  onChanged: _handleRadioValueChanged,
-                  activeColor: Colors.black,
-                ),
+                // Radio(
+                //   value: 0,
+                //   groupValue: _radioGroupA,
+                //   onChanged: _handleRadioValueChanged,
+                //   activeColor: Colors.black,//被选中的颜色
+                // ),
+                // Radio(
+                //   value: 1,
+                //   groupValue: _radioGroupA,
+                //   onChanged: _handleRadioValueChanged,
+                //   activeColor: Colors.black,
+                // ),
               ],
             ),
           ],
