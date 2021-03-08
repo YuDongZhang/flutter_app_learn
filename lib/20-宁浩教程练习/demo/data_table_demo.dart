37b471316ff7b7e80c79093a8f970be14c122ca1
YuDongZhang <977872239@qq.com>
2021-03-08 17:26:50 +0800

    DataTable：数据表格的排序

--- lib/20-宁浩教程练习/demo/data_table_demo.dart
+++ lib/20-宁浩教程练习/demo/data_table_demo.dart
@@ -7,6 +7,9 @@ class DataTableDemo extends StatefulWidget {
 }
 
 class _DataTableDemoState extends State<DataTableDemo> {
+  int _sortColumnIndex;
+  bool _sortAscending = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +22,31 @@ class _DataTableDemoState extends State<DataTableDemo> {
           child: ListView(
             children: <Widget>[
               DataTable(
+                sortColumnIndex: _sortColumnIndex, //排序的索引号
+                sortAscending: _sortAscending, //true 升序排列 , false 降序
                 columns: [
                   //数据表格的栏目
                   DataColumn(
-                    label: Text('Title'), //container 包含设置宽度 , 好像都不能自己设置宽度
-                  ),
+                      label: Text('Title'), //container 包含设置宽度 , 好像都不能自己设置宽度
+                      onSort: (int index, bool ascending) {
+                        //index当前要排序的索引号 ,ascending 是否升序排列
+                        setState(() {
+                          _sortColumnIndex = index;
+                          _sortAscending = ascending;
+                          //sort 这个方法里面的方法就是排序的方法
+                          posts.sort((a, b) {
+                            //调换 a 和 b 的值
+                            if (!ascending) {
+                              final c = a;
+                              a = b;
+                              b = c;
+                            }
+                            //返回对比的结果
+                            return a.title.length
+                                .compareTo(b.title.length); //比较标题的长度
+                          });
+                        });
+                      }),
                   DataColumn(
                     label: Text('Author'),
                   ),
