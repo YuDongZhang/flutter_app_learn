41b4f341e4356257ac9410a1fbc648444cd01a6d
YuDongZhang <977872239@qq.com>
2021-03-08 15:35:43 +0800

    chip 小碎片 , 小标签

---
+++ lib/20-宁浩教程练习/demo/chip_demo.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class ChipDemo extends StatefulWidget {
+  @override
+  _ChipDemoState createState() => _ChipDemoState();
+}
+
+class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tags = [
+    'Apple',
+    'Banana',
+    'Lemon',
+  ];
+
+  String _action = 'Nothing';
+  List<String> _selected = [];
+  String _choice = 'Lemon';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChipDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: <Widget>[
+                Chip(
+                  label: Text('Life'),
+                ),
+                Chip(
+                  label: Text('Sunset'),
+                  backgroundColor: Colors.orange,
+                ),
+                Chip(
+                  label: Text('Wanghao'),
+                  avatar: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Text('皓'),
+                  ),
+                ),
+                Chip(
+                  label: Text('Wanghao'),
+                  avatar: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://resources.ninghao.net/images/wanghao.jpg'),
+                  ),
+                ),
+                Chip(
+                  label: Text('City'),
+                  onDeleted: () {},
+                  deleteIcon: Icon(Icons.delete),
+                  deleteIconColor: Colors.redAccent,
+                  deleteButtonTooltipMessage: 'Remove this tag',
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  // indent: 32.0,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: () {
+          setState(() {
+            _tags = [
+              'Apple',
+              'Banana',
+              'Lemon',
+            ];
+
+            _selected = [];
+
+            _choice = 'Lemon';
+          });
+        },
+      ),
+    );
+  }
+}
