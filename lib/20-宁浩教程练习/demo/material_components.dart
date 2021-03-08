bceca888ded8e580a35e6cd135175e47fc282153
YuDongZhang <977872239@qq.com>
2021-03-08 09:54:31 +0800

    CheckboxListTile：带标签与图标的复选框

--- lib/20-宁浩教程练习/demo/material_components.dart
+++ lib/20-宁浩教程练习/demo/material_components.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/demo/checkbox_demo.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/demo/form_demo.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/demo/popup_menu_button_demo.dart';
+import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/demo/radio_demo.dart';
 
 import 'Button_Demo.dart';
 import 'floating_action_button_demo.dart';
@@ -16,6 +17,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          ListItem(title: 'radiobutton', page: RadioDemo()),
           ListItem(title: 'CheckBoxDemo', page: CheckboxDemo()),
           ListItem(title: 'form', page: FormDemo()),
           ListItem(title: 'PopupMenuButton', page: PopupMenuButtonDemo()),
---
+++ lib/20-宁浩教程练习/demo/radio_demo.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class RadioDemo extends StatefulWidget {
+  @override
+  _RadioDemoState createState() => _RadioDemoState();
+}
+
+class _RadioDemoState extends State<RadioDemo> {
+  int _radioGroupA = 0;
+  
+  void _handleRadioValueChanged(int value) {
+    setState(() {
+      _radioGroupA = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RadioDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('RadioGroupValue: $_radioGroupA'),
+            SizedBox(height: 32.0),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Radio(
+                  value: 0,
+                  groupValue: _radioGroupA,
+                  onChanged: _handleRadioValueChanged,
+                  activeColor: Colors.black,//被选中的颜色
+                ),
+                Radio(
+                  value: 1,
+                  groupValue: _radioGroupA,
+                  onChanged: _handleRadioValueChanged,
+                  activeColor: Colors.black,
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
