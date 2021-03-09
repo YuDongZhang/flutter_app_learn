4293cc96ab3d689c35f1e36843a37c3482eb6111
YuDongZhang <977872239@qq.com>
2021-03-09 11:39:59 +0800

    创建与使用 InheritedWidget 有效地传递数据给子部件

--- lib/20-宁浩教程练习/demo/state/state_management_demo.dart
+++ lib/20-宁浩教程练习/demo/state/state_management_demo.dart
@@ -18,46 +18,67 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0,
-      ),
-      body: CounterWrapper(_count, _increaseCount),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _increaseCount,
+    return CounterProvider(
+      //这样在这个部件下的小部件都可以得到 这个两个参数
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0,
+        ),
+        body: CounterWrapper(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _increaseCount,
+        ),
       ),
     );
   }
 }
 
 class CounterWrapper extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount; //回调
-
-  CounterWrapper(this.count, this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Counter(count, increaseCount),
+      child: Counter(),
     );
   }
 }
 
 class Counter extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount; //回调
-
-  Counter(this.count, this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
+    final int count = CounterProvider
+        .of(context)
+        .count;
+    final VoidCallback increaseCount = CounterProvider
+        .of(context)
+        .increaseCount;
     return ActionChip(
       label: Text('$count'),
       onPressed: increaseCount, //执行的是一个回调
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount; //回调
+  final Widget child;
+
+  CounterProvider({this.count, this.increaseCount, this.child})
+      : super(child: child); //
+
+  //在其他的小部件中采用 of 得到state
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
+
+  //通知继承这个小部件的小部件 , 是否重建
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
   }
 }
