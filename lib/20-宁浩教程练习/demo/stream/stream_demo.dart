6ae14d5ab931755720355987b9814bd570421291
YuDongZhang <977872239@qq.com>
2021-03-10 18:21:22 +0800

    15.1stream项目

---
+++ lib/20-宁浩教程练习/demo/stream/stream_demo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class StreamDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        elevation: 0.0,
+      ),
+      body: StreamDemoHome(),
+    );
+  }
+}
+
+// stless
+// stful
+
+class StreamDemoHome extends StatefulWidget {
+  @override
+  _StreamDemoHomeState createState() => _StreamDemoHomeState();
+}
+
+class _StreamDemoHomeState extends State<StreamDemoHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
