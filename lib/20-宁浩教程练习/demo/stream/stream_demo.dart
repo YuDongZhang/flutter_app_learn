e2cfb21c9ad48621d9e86ef0845c050513700b81
zhangyudong <977872239@qq.com>
2021-03-10 19:58:00 +0800

    15.4 创建与监听 Stream_

--- lib/20-宁浩教程练习/demo/stream/stream_demo.dart
+++ lib/20-宁浩教程练习/demo/stream/stream_demo.dart
@@ -22,8 +22,33 @@ class StreamDemoHome extends StatefulWidget {
 }
 
 class _StreamDemoHomeState extends State<StreamDemoHome> {
+
+  @override
+  void initState() {
+    super.initState();
+    print('create stream');
+    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    _streamDemo.listen(onData);
+    print('end');
+  }
+
+  void  onData(String data) {
+    print('$data');
+  }
+
+  Future<String> fetchData() async{
+    await Future.delayed(Duration(seconds: 3));//模拟网络请求
+    return 'hello';//要使用这个的数据 , 就要监听 stream
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+
+
+
+
+
+
 }
