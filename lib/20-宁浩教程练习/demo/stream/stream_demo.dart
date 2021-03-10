ecf8a6be8e6ac7cd5d5e5cd7548c432b650a50e2
zhangyudong <977872239@qq.com>
2021-03-10 20:17:50 +0800

    6 暂停、恢复、取消监听 Stream

--- lib/20-宁浩教程练习/demo/stream/stream_demo.dart
+++ lib/20-宁浩教程练习/demo/stream/stream_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StreamDemo extends StatelessWidget {
@@ -22,6 +24,7 @@ class StreamDemoHome extends StatefulWidget {
 }
 
 class _StreamDemoHomeState extends State<StreamDemoHome> {
+  StreamSubscription _streamDemoSubscription;
 
   @override
   void initState() {
@@ -29,9 +32,9 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
     print('create stream');
     Stream<String> _streamDemo = Stream.fromFuture(fetchData());
     //有数据 ondata , 错误 onerror ,完成 onDone
-    _streamDemo.listen(onData,onError: onError,onDone: onDone);
+    _streamDemoSubscription =
+        _streamDemo.listen(onData, onError: onError, onDone: onDone);
     print('end');
-
   }
 
   void onDone() {
@@ -42,20 +45,44 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
     print('$error');
   }
 
-  void  onData(String data) {
+  void onData(String data) {
     print('$data');
   }
 
-  Future<String> fetchData() async{
-    await Future.delayed(Duration(seconds: 3));//模拟网络请求
+  void _pauseStream() {
+    print('Pause subscription');
+    _streamDemoSubscription.pause();
+  }
+
+  void _resumeStream() {
+    print('Resume subscription');
+    _streamDemoSubscription.resume();
+  }
+
+  void _cancelStream() {
+    print('Cancel subscription');
+    _streamDemoSubscription.cancel();
+  }
+
+  Future<String> fetchData() async {
+    await Future.delayed(Duration(seconds: 3)); //模拟网络请求
     throw 'something happened'; //这里抛出错误的时候 , 就会在 onerroe 方法中执行
-    return 'hello';//要使用这个的数据 , 就要监听 stream
+    return 'hello'; //要使用这个的数据 , 就要监听 stream
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton(onPressed: _pauseStream, child: Text('Pause')),
+            FlatButton(onPressed: _resumeStream, child: Text('Resume')),
+            FlatButton(onPressed: _cancelStream, child: Text('Cancel')),
+          ],
+        ),
+      ),
+    );
   }
-
-
 }
