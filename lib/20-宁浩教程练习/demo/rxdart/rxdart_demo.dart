3478f8703d9183527e3b437454a00192b96ec115
zhangyudong <977872239@qq.com>
2021-03-11 01:48:39 +0800

    11 准备一个文本框往 Observable 上添加数据_

--- lib/20-宁浩教程练习/demo/rxdart/rxdart_demo.dart
+++ lib/20-宁浩教程练习/demo/rxdart/rxdart_demo.dart
@@ -20,9 +20,13 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  PublishSubject<String> _textFieldSubject;
+
   @override
   void initState() {
     super.initState();
+    _textFieldSubject = PublishSubject<String>();
+    _textFieldSubject.listen((data) => print(data));
 
     // Observable<String> _observable =
     // Observable(Stream.fromIterable(['hello', '您好']));//可迭代的数据
@@ -40,21 +44,45 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     //     BehaviorSubject<String>(); //把最后一次添加的数据作为新的数据交给新来的监听
 
     //数据全部交给监听器
-    ReplaySubject<String> _subject =
-        ReplaySubject<String>(maxSize: 2); //maxsize 最大接收的数量,也可以没有,第一个收不到
-
-    _subject.add('hello');
-    _subject.add('hola');
-    _subject.add('hi');
+    // ReplaySubject<String> _subject =
+    //     ReplaySubject<String>(maxSize: 2); //maxsize 最大接收的数量,也可以没有,第一个收不到
+    //
+    // _subject.add('hello');
+    // _subject.add('hola');
+    // _subject.add('hi');
 
     //监听 stream 或者observable ,如果stream需要有数据要写一个方法 如下 .  这个相当于订阅
-    _subject.listen((data) => print('listen 1: $data'));
-    _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
-    _subject.close();
+    // _subject.listen((data) => print('listen 1: $data'));
+    // _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
+    // _subject.close();
+  }
+
+  @override
+  void dispose() {
+    _textFieldSubject.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: Colors.black,
+      ),
+      child: TextField(
+        onChanged: (value) {
+          //数据变化时方法
+          _textFieldSubject.add('input: $value');
+        },
+        onSubmitted: (value) {
+          //提交时的方法
+          _textFieldSubject.add('submit: $value');
+        },
+        decoration: InputDecoration(
+          labelText: 'Title',
+          filled: true,
+        ),
+      ),
+    );
   }
 }
