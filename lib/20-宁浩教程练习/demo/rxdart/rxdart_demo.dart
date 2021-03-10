b4fcc4145b1292ad2b5c8237f977e622994c1963
zhangyudong <977872239@qq.com>
2021-03-11 01:30:42 +0800

    8 使用 Subjects 控制 Observables

--- lib/20-宁浩教程练习/demo/rxdart/rxdart_demo.dart
+++ lib/20-宁浩教程练习/demo/rxdart/rxdart_demo.dart
@@ -24,15 +24,25 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
   void initState() {
     super.initState();
 
-    Observable<String> _observable =
-        // Observable(Stream.fromIterable(['hello', '您好']));//可迭代的数据
-        // Observable.fromFuture(Future.value('hello ~'));//返回一个 future
-        // Observable.fromIterable(['hello', '您好']);
-        // Observable.just('hello ~');
-        Observable.periodic(
-            Duration(seconds: 3), (x) => x.toString()); //指定时间间隔重复触发
+    // Observable<String> _observable =
+    // Observable(Stream.fromIterable(['hello', '您好']));//可迭代的数据
+    // Observable.fromFuture(Future.value('hello ~'));//返回一个 future
+    // Observable.fromIterable(['hello', '您好']);
+    // Observable.just('hello ~');
+    //     Observable.periodic(
+    //         Duration(seconds: 3), (x) => x.toString()); //指定时间间隔重复触发
+    //
+    // _observable.listen(print);
 
-    _observable.listen(print);
+    //类似于 streamcontrol
+    PublishSubject<String> _subject = PublishSubject<String>();
+
+    //监听 stream 或者observable ,如果stream需要有数据要写一个方法 如下 .  这个相当于订阅
+    _subject.listen((data) => print('listen 1: $data'));
+    _subject.add('hello');
+    _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
+    _subject.add('hola');
+    _subject.close();
   }
 
   @override
