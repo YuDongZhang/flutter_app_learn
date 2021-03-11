25f8216aa8f611be3047c7bd91fd5db3084a8d95
YuDongZhang <977872239@qq.com>
2021-03-11 17:08:40 +0800

    5 BLoC 应用案例（记数器）：准备基本小部件

---
+++ lib/20-宁浩教程练习/demo/bloc/counter_bloc_demo.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class CounterHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ActionChip(
+        label: Text('0'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class CounterActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(child: Icon(Icons.add), onPressed: () {});
+  }
+}
