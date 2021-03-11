796b46f116473c3baa5b7442e8ae74ddad8fa57b
YuDongZhang <977872239@qq.com>
2021-03-11 19:56:56 +0800

    8 创建与使用 BLoC：用 Stream 输出数据

--- lib/20-宁浩教程练习/demo/bloc/counter_bloc_demo.dart
+++ lib/20-宁浩教程练习/demo/bloc/counter_bloc_demo.dart
@@ -7,13 +7,22 @@ class CounterHome extends StatelessWidget {
   Widget build(BuildContext context) {
     CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return Center(
-      child: ActionChip(
-        label: Text('0'),
-        onPressed: () {
-          _counterBloc.log();
-        },
-      ),
-    );
+        child: StreamBuilder(
+      initialData: 0,
+
+      ///4.根据这个数据构建了下面的小部件 , 与变化自动更新
+      stream: _counterBloc.count,
+
+      builder: (context, snapshot) {
+        return ActionChip(
+            label: Text('${snapshot.data}'),
+            //data是 _counterBloc.count上 stream里的数据
+            onPressed: () {
+              ///1,按下按钮会使用counter里sink往stream加数据
+              _counterBloc.counter.add(1);
+            });
+      },
+    ));
   }
 }
 
@@ -49,20 +58,32 @@ class CounterProvider extends InheritedWidget {
 }
 
 class CounterBloc {
+  int _count = 0;
+
   final _counterActionController = StreamController<int>();
 
   StreamSink<int> get counter => _counterActionController.sink;
 
+  final _counterController = StreamController<int>();
+
+  Stream<int> get count => _counterController.stream;
+
   CounterBloc() {
+    ///2 , 监听了数据 , 有数据会执行 ondata 方法 ,
     _counterActionController.stream.listen(onData); //监听
   }
 
   void onData(int data) {
     print('$data');
+    _count = data + _count;
+
+    ///3 , 改变了值将值交给_counterController控制的stream
+    _counterController.add(_count);
   }
 
   void disponse() {
     _counterActionController.close();
+    _counterController.close();
   }
 
   void log() {
