95c26f60511e39ea1a0cb261d1439eb65747a15f
YuDongZhang <977872239@qq.com>
2021-03-12 11:37:32 +0800

    18.5 使用 http 客户端请求数据

--- lib/20-宁浩教程练习/demo/http/http_demo.dart
+++ lib/20-宁浩教程练习/demo/http/http_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HttpDemo extends StatelessWidget {
   @override
@@ -8,6 +9,31 @@ class HttpDemo extends StatelessWidget {
         title: Text('HttpDemo'),
         elevation: 0,
       ),
+      body: HttpDemoHome(),
     );
+  }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+    fetchPost();
+  }
+
+  void fetchPost() async {
+    final response =
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    print('statusCode : ${response.statusCode}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
