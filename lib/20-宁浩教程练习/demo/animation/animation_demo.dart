487866fda54e6ee9cfe67d073f20a1ccaacdfd1e
YuDongZhang <977872239@qq.com>
2021-03-12 15:14:09 +0800

    19.4 AnimationController：动画控制器

--- lib/20-宁浩教程练习/demo/animation/animation_demo.dart
+++ lib/20-宁浩教程练习/demo/animation/animation_demo.dart
@@ -18,7 +18,36 @@ class AnimationDemoHome extends StatefulWidget {
   _AnimationDemoHomeState createState() => _AnimationDemoHomeState();
 }
 
-class _AnimationDemoHomeState extends State<AnimationDemoHome> {
+class _AnimationDemoHomeState extends State<AnimationDemoHome>
+    with TickerProviderStateMixin {
+  AnimationController animationDemoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationDemoController = AnimationController(
+      duration: Duration(milliseconds: 1000), //持续的时间
+      //防止屏幕的消耗不必要的资源
+      vsync: this,
+    );
+
+    //监听动画
+    animationDemoController.addListener(() {
+      //输出每一帧 控制的值
+      print('$animationDemoController.value');
+    });
+
+    //开始播放动画
+    animationDemoController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationDemoController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
