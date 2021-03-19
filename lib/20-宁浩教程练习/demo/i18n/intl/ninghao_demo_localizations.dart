d2a698c61ce47225c26b1bba202bf397d2f6c3d1
YuDongZhang <977872239@qq.com>
2021-03-19 15:00:33 +0800

    20.13 提取信息模板并翻译信息

--- lib/20-宁浩教程练习/demo/i18n/intl/ninghao_demo_localizations.dart
+++ lib/20-宁浩教程练习/demo/i18n/intl/ninghao_demo_localizations.dart
@@ -11,3 +11,7 @@ class NinghaoDemoLocalizetions {
         args: [name],
       );
 }
+
+//生成 arb文件
+//C:\AndroidStudioProjects\flutter_app_learn>flutter pub pub run intl_translation:extract_to_arb --output-dir=20-宁浩
+// 教程练习/demo/i18n/intl  20-宁浩教程练习/demo/i18n/intl/ninghao_demo_localizations.dart
