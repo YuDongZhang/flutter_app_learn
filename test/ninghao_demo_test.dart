5da365b004482b0e8e094205cfd8e5dfbf518d4a
YuDongZhang <977872239@qq.com>
2021-03-22 14:41:27 +0800

    21.5.WidgetTesting：测试小部件

--- test/ninghao_demo_test.dart
+++ test/ninghao_demo_test.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_app_learn/20-%E5%AE%81%E6%B5%A9%E6%95%99%E7%A8%8B%E7%BB%83%E4%B9%A0/demo/test_demo.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -8,5 +9,12 @@ void main() {
     var str = NinghaoTestDemo.greet('ninghao');
     //断言
     expect(str, 'hello ninghao');
+  });
+
+  //也就是测试小部件在不在某个地方
+  testWidgets('测试小部件', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: TestDemo(),
+    ));
   });
 }
