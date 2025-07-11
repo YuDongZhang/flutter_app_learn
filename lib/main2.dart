47dec54bb8851add846967a05818f1205000a325
zhangyudong <977872239@qq.com>
2025-07-11 11:40:00 +0800

    老项目跑起来了

--- lib/main2.dart
+++ lib/main2.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -66,8 +65,6 @@ class ColumnToColumnPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text("widget"),
         ),
         body: Container(
